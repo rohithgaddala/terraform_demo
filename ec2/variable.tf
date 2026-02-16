4f71ec6f393f46baf1c63cdf4b3f3ec1683045ec
Rohith.G <130386995+rohithgaddala@users.noreply.github.com>
2026-02-16 11:24:11 +0530

    Add variables for AMI ID, instance type, and name

---
+++ ec2/variable.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance Type"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Instance Name"
+  type        = string
+}
